7cb86a64cf8696f4b91ffa0b4fbe7bceef1c9bb3
Amir Ali shar <141339125+AmirAliShar057@users.noreply.github.com>
2023-09-02 10:09:03 +0500

    Update triangle.java

--- src/triangle.java
+++ src/triangle.java
@@ -1,25 +1,20 @@
 public class triangle {
     public static void main(String[] args) {
       /*  for (int i=1;i<=5;i++){
-            for (int j=5;j>=i;j--){                System.out.print(j+" ");
+            for (int j=5;j>=i;j--){ 
+            System.out.print(j+" ");
             }
             System.out.println();
         }
 
        */
-      /*  for (int i=1;i<=5;i++){
+        //second triangle
+       for (int i=1;i<=5;i++){
             for (int j=5;j>i;j--){
                 System.out.print(" ");
             }for (int k=1;k<=i;k++){
             System.out.print(k);
         }
             System.out.println();}
-   */
-    int sum=0;
-    for (int i=1;i<=1000;i++){
-        sum=sum+i;
-    }
-        System.out.println(sum);
-    }
-
+}
 }
